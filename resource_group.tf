29febfe9d94a54033d7f833f22555ff323aef820
Kevin <kevin@smithvoth.ca>
2026-05-02 20:35:52 -0400

    Kickstart workflow

--- resource_group.tf
+++ resource_group.tf
@@ -1,8 +1,10 @@
 
+
 resource "azurerm_resource_group" "cacthis" {
   name     = "tfc-cac-oidc-working-rg"
   location = "canadacentral"
 }
+
 
 resource "azurerm_resource_group" "caethis" {
   name     = "tfc-cae-oidc-working-rg"
